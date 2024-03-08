068ec44b507163170f9ba964e0005c5bb430dbd4
B166er01 <b166erpro@gmail.com>
2024-03-08 13:19:11 +0100

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-    </main>
+    <main className="w-full mx-auto">
+
+    {/* Header section */}
+    <div className="w-full mb-8 text-center">
+      <h1 className="text-4xl md:text-5xl">La Voie De L&rsquo;Info</h1>
+      <p className="text-lg">Votre fenêtre sur l&rsquo;actualité</p>
+    </div>
+
+  </main>
   );
 }
