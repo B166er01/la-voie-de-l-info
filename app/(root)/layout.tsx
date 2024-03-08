0a248955932e787c664c5c521868342bf640b864
B166er01 <b166erpro@gmail.com>
2024-03-08 18:15:34 +0100

    layout & cat

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Navigation from '@/components/layout/Navigation'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div>
+      <Navigation />
+      <div className="max-w-screen-xl mx-auto mt-24">{children}</div>
+    </div>
+  )
+}
