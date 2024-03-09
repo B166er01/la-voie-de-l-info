b63ba1ed9aae7b4eb70a183d3b01dfca3ffbbacb
B166er01 <b166erpro@gmail.com>
2024-03-09 19:27:26 +0100

    layout & cat

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,24 @@
-import Image from "next/image";
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  return <div>
-    <nav className="w-full h-18 absolute top-0 p-3 flex gap-3 items-center">
-      <div className="w-[40px] h-[32px]">
-      <Image width={100} height={80} alt="" src={"/logo-news-s.png"} />
-      </div>
-      <div className="capitalize text-2xl">la voie de l&rsquo;info</div>
-  </nav>
-    {children}
-  </div>
+  return (
+    <div>
+      <nav className="absolute top-0 flex items-center w-full gap-3 h-18">
+ 
+          <Link href={"/"} className='flex flex-col items-center justify-center w-full pt-3 scale-75 lg:pt-0'>    
+
+              <h1 className="text-4xl lg:text-5xl">La Voie De L&rsquo;Info</h1>
+              <p className="hidden text-lg lg:inline-block">Votre fenêtre sur l&rsquo;actualité</p>
+
+      </Link>
+ 
+      </nav>
+      {children}
+    </div>
+  )
 }
