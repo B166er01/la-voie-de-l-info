1ab06faef84f6270d59bc1e561e3e8d83e71aad5
B166er01 <b166erpro@gmail.com>
2024-03-09 19:28:41 +0100

    layout & cat

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function RootLayout({
@@ -9,14 +8,15 @@ export default function RootLayout({
   return (
     <div>
       <nav className="absolute top-0 flex items-center w-full gap-3 h-18">
- 
-          <Link href={"/"} className='flex flex-col items-center justify-center w-full pt-3 scale-75 lg:pt-0'>    
-
-              <h1 className="text-4xl lg:text-5xl">La Voie De L&rsquo;Info</h1>
-              <p className="hidden text-lg lg:inline-block">Votre fenêtre sur l&rsquo;actualité</p>
-
-      </Link>
- 
+        <Link
+          href={'/'}
+          className="flex flex-col items-center justify-center w-full pt-3 scale-75 lg:pt-0"
+        >
+          <h1 className="text-4xl lg:text-5xl">La Voie De L&rsquo;Info</h1>
+          <p className="hidden text-lg lg:inline-block">
+            Votre fenêtre sur l&rsquo;actualité
+          </p>
+        </Link>
       </nav>
       {children}
     </div>
